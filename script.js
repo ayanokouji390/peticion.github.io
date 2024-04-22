dc5c5a7c3ab597daf5534ab6aa1a9f6266cdd3f3
ayanokouji390 <167820112+ayanokouji390@users.noreply.github.com>
2024-04-22 17:31:09 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("sin").addEventListener("click", function() {
+
+    document.getElementById("sin").style.display = "none";
+    document.getElementById("peticion").style.display = "none";
+    document.getElementById("carta").style.visibility = "visible";
+    
+});
